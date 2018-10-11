15967394d4358a78dfd21989f8bd971be3333eb0
rajhaq <zubaer.haque@gmail.com>
2018-10-11 17:10:46 +0600

    menu mod

--- resources/assets/js/components/mainapp.vue
+++ resources/assets/js/components/mainapp.vue
@@ -91,7 +91,7 @@
                             <MenuItem name="1-1" :to="handleGoToMenu('/newbooking')">New Booking</MenuItem>
                             <MenuItem name="1-2" :to="handleGoToMenu('/bookinglist')">Booking List</MenuItem>
                             <MenuItem name="1-3" :to="handleGoToMenu('/calender')" >Calender</MenuItem>
-                            <MenuItem name="1-5">Compleated Events</MenuItem>
+                            <MenuItem name="1-4">Compleated Events</MenuItem>
                         </Submenu>
                         <Submenu name="2">
                             <template slot="title">
@@ -105,7 +105,7 @@
                 <Layout :style="{padding: '0 24px 24px'}">
                     <Breadcrumb :style="{margin: '24px 0'}">
                         <BreadcrumbItem>Home</BreadcrumbItem>
-                        <BreadcrumbItem>Home</BreadcrumbItem>
+                        <BreadcrumbItem>Item</BreadcrumbItem>
                     </Breadcrumb>
                     <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
                         <router-view></router-view>
